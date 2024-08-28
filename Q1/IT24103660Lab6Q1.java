a7af2e7aa63a159eb996d6e649b8e7e3f167fb40
it24103660 <it24103660@my.sliit.lk>
2024-08-28 09:40:54 +0530

    Add files via upload

---
+++ Q1/IT24103660Lab6Q1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ public class IT24103660Lab6Q1{
+	 public static void main(String []args){
+		 Scanner sc = new Scanner(System.in);
+		 
+		 System.out.println("Enter a number: ");
+		 double number = sc.nextDouble();
+		 
+		 double square = number*number;
+		 System.out.println("The square of "+number+" is: " +square);
+		 
+		 double squareroot = Math.sqrt(number);
+		  System.out.println("The square of "+number+" is: " +squareroot);
+	 }
+ }
+		 
+		 
