b6e308100a34685154a830670beb04bf16c4eece
it24103660 <it24103660@my.sliit.lk>
2024-08-28 10:19:21 +0530

    Add files via upload

---
+++ Q1/IT24103660Lab6Q2A.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class IT24103660Lab6Q2A{
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+		int Count = 0;
+		
+		while(Count<10){
+			Count = Count + 1;
+		System.out.print(" "+Count+" ");
+		}
+		
+	}
+}
